cd6b6578382c0903380649622b12159d34f6d470
codesoap <codesoap@mailbox.org>
2020-12-20 13:07:34 +0100

    Implement the generation of subaddresses

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ package main
 
 import (
 	"crypto/rand"
+	"encoding/binary"
+	"flag"
 	"fmt"
 	"math/big"
 	"os"
@@ -16,6 +18,7 @@ import (
 )
 
 var l *big.Int
+var nSubadresses = flag.Int("s", 0, "Number of subaddresses to generate.")
 
 func init() {
 	var ok bool
@@ -27,6 +30,7 @@ func init() {
 }
 
 func main() {
+	flag.Parse()
 	secretSpendKey, err := generateSecretSpendKey()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error while generating secret spend key: %v\n", err)
@@ -47,15 +51,28 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error while deriving public view key: %v\n", err)
 		os.Exit(1)
 	}
-	address, err := deriveAddress(publicSpendKey, publicViewKey)
+	address, err := deriveAddress(publicSpendKey, publicViewKey, 18)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error while deriving address: %v\n", err)
 		os.Exit(1)
+	}
+	subaddresses := make([]string, 0)
+	// Subaddress 0 is also skipped in the official wallet:
+	for i := 1; nSubadresses != nil && i <= *nSubadresses; i++ {
+		subaddress, err := deriveSubaddress(secretViewKey, publicSpendKey, uint32(0), uint32(i))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error while deriving subaddress: %v\n", err)
+			os.Exit(1)
+		}
+		subaddresses = append(subaddresses, subaddress)
 	}
 
 	fmt.Printf("secret spend key: %s\n", toString(secretSpendKey))
 	fmt.Printf("secret view key : %s\n", toString(secretViewKey))
 	fmt.Printf("primary address : %s\n", address)
+	for i, subaddress := range subaddresses {
+		fmt.Printf("subaddress #%04d: %s\n", i+1, subaddress)
+	}
 }
 
 func generateSecretSpendKey() (*big.Int, error) {
@@ -91,13 +108,13 @@ func derivePublicKey(secretKey *big.Int) (*big.Int, error) {
 	return big.NewInt(0).SetBytes(p.Bytes()), nil
 }
 
-func deriveAddress(publicSpendKey, publicViewKey *big.Int) (string, error) {
+func deriveAddress(publicSpendKey, publicViewKey *big.Int, networkByte byte) (string, error) {
 	publicSpendKeyBytes := make([]byte, 32, 32)
 	publicViewKeyBytes := make([]byte, 32, 32)
 	publicSpendKey.FillBytes(publicSpendKeyBytes)
 	publicViewKey.FillBytes(publicViewKeyBytes)
 	bytes := make([]byte, 0, 69)
-	bytes = append(bytes, 0x12) // Monero network byte.
+	bytes = append(bytes, networkByte)
 	bytes = append(bytes, publicSpendKeyBytes...)
 	bytes = append(bytes, publicViewKeyBytes...)
 	hasher := sha3.NewLegacyKeccak256()
@@ -107,6 +124,64 @@ func deriveAddress(publicSpendKey, publicViewKey *big.Int) (string, error) {
 	hashBytes := hasher.Sum(nil)
 	bytes = append(bytes, hashBytes[:4]...)
 	return moneroAddressEncode(bytes), nil
+}
+
+// deriveSubaddress derives the subaddress for account iAcc and index
+// iSub for the standard address' secretViewKey and publicSpendKey.
+// See https://monerodocs.org/public-address/subaddress/.
+func deriveSubaddress(secretViewKey, publicSpendKey *big.Int, iAcc, iSub uint32) (string, error) {
+	m, err := deriveM(secretViewKey, iAcc, iSub)
+	if err != nil {
+		return "", fmt.Errorf("could not derive m: %w", err)
+	}
+	publicSpendKeyBytes := make([]byte, 32, 32)
+	publicSpendKey.FillBytes(publicSpendKeyBytes)
+	B, err := edwards25519.NewIdentityPoint().SetBytes(publicSpendKeyBytes)
+	if err != nil {
+		return "", err
+	}
+	D := B.Add(B, edwards25519.NewIdentityPoint().ScalarBaseMult(m))
+	subPublicSpendKey := big.NewInt(0).SetBytes(D.Bytes())
+
+	secretViewKeyBytes := make([]byte, 32, 32)
+	secretViewKey.FillBytes(secretViewKeyBytes)
+	a, err := edwards25519.NewScalar().SetCanonicalBytes(secretViewKeyBytes)
+	if err != nil {
+		return "", err
+	}
+	C := edwards25519.NewIdentityPoint().ScalarMult(a, D)
+	subPublicViewKey := big.NewInt(0).SetBytes(C.Bytes())
+
+	return deriveAddress(subPublicSpendKey, subPublicViewKey, 42)
+}
+
+func deriveM(secretViewKey *big.Int, iAcc, iSub uint32) (*edwards25519.Scalar, error) {
+	hasher := sha3.NewLegacyKeccak256()
+	if _, err := hasher.Write([]byte("SubAddr\u0000")); err != nil {
+		return edwards25519.NewScalar(), err
+	}
+	secretViewKeyBytes := make([]byte, 32, 32)
+	secretViewKey.FillBytes(secretViewKeyBytes)
+	if _, err := hasher.Write(secretViewKeyBytes); err != nil {
+		return edwards25519.NewScalar(), err
+	}
+	buf := make([]byte, 4, 4)
+	binary.LittleEndian.PutUint32(buf, iAcc)
+	if _, err := hasher.Write(buf); err != nil {
+		return edwards25519.NewScalar(), err
+	}
+	buf = make([]byte, 4, 4)
+	binary.LittleEndian.PutUint32(buf, iSub)
+	if _, err := hasher.Write(buf); err != nil {
+		return edwards25519.NewScalar(), err
+	}
+	mInt := big.NewInt(0).SetBytes(hasher.Sum(nil))
+	revertBytes(mInt)
+	mInt.Mod(mInt, l)
+	revertBytes(mInt)
+	mBytes := make([]byte, 32, 32)
+	mInt.FillBytes(mBytes)
+	return edwards25519.NewScalar().SetCanonicalBytes(mBytes)
 }
 
 func moneroAddressEncode(bytes []byte) (out string) {
